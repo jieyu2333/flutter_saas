ccecb7a032b1a6d62866a42db111496c9244ec22
Tiger <atlantisspeed@gmail.com>
2019-08-28 10:37:01 +0800

    忘记密码

---
+++ lib/view/forget_password_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ForgetPasswordPage extends StatefulWidget {
+  @override
+  _ForgetPasswordPageState createState() => _ForgetPasswordPageState();
+}
+
+class _ForgetPasswordPageState extends State<ForgetPasswordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
