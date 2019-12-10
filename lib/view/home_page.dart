c4edb668ce036f10fd1aba3cae05b7795a1a45b0
Tiger <atlantisspeed@gmail.com>
2019-12-10 22:15:58 +0800

    首页

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  _HomePageState createState() => _HomePageState(
-          items: List<ListItem>.generate(
-        1000,
-        (i) => i % 6 == 0
-            ? HeadingItem("Heading $i")
-            : GardItem("Sender $i", "Message body $i"),
-      ));
+  _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<ListItem> items;
+  final List<Object> items;
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
 
   _HomePageState({Key key, @required this.items});
 
@@ -21,53 +33,44 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('主页'),
+        title: const Text('BottomNavigationBar Sample'),
       ),
-      body: ListView.builder(
-          itemCount: items.length,
-          // ignore: missing_return
-          itemBuilder: (context, index) {
-            final item = items[index];
-            if (item is HeadingItem) {
-              return ListTile(
-                title: Text(
-                  item.heading,
-                  style: Theme.of(context).textTheme.headline,
-                ),
-              );
-            } else if (item is GardItem) {
-              return ListTile(
-                title: Text(item.sender),
-                subtitle: Text(item.body),
-              );
-            }
-          }),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('首页'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('值得买'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            title: Text('购物车'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('个人'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
-}
-
-abstract class ListItem {}
-
-class HeadingItem implements ListItem {
-  final String heading;
-
-  HeadingItem(this.heading);
-}
-
-class MessageItem implements ListItem {
-  final String sender;
-  final String body;
-
-  MessageItem(this.sender, this.body);
-}
-
-class GardItem implements ListItem {
-  final String sender;
-  final String body;
-
-  GardItem(this.sender, this.body);
 }
